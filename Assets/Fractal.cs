f27747b96dfc76827a7869c571c3ec112b2e3eef
xCmac <dsemac@gmail.com>
2014-12-07 23:02:21 -0600

    Making some Fractals

--- Assets/Fractal.cs
+++ Assets/Fractal.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Fractal : MonoBehaviour {
+public class Fractal : MonoBehaviour
+{
+    private int depth;
+
+    public Mesh mesh;
+    public Material material;
+	public int maxDepth;
+	public float childScale;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+	    gameObject.AddComponent<MeshFilter>().mesh = mesh;
+	    gameObject.AddComponent<MeshRenderer>().material = material;
+	    if (depth < maxDepth)
+	    {
+	        new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.up);
+			new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.right);
+	    }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	private void Initialize (Fractal parent, Vector3 direction)
+	{
+		mesh = parent.mesh;
+		material = parent.material;
+		maxDepth = parent.maxDepth;
+		depth = parent.depth + 1;
+		childScale = parent.childScale;
+		transform.parent = parent.transform;
+		transform.localScale = Vector3.one * childScale;
+		transform.localPosition = direction * (0.5f + 0.5f * childScale);
 	}
 }
